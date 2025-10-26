c2450e1dc5cbf7ccd27b5f3ad63ccb1a125713ef
Jang seheon <tpgjs3121@naver.com>
2025-10-27 00:47:46 +0900

    feat: 랜덤 숫자를 통한 조건 만족시 전진 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,14 +1,27 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
         System.out.println("시도할 횟수는 몇 회인가요?");
-        Long count = Long.parseLong(Console.readLine());
+        long count = Long.parseLong(Console.readLine());
         String[] cars = input.split(",");
-        
+        long[] goCount = new long[cars.length];
+        int i = 0;
+        while(i < count){
+            for(int j = 0; j < cars.length; j++){
+                int random = Randoms.pickNumberInRange(0,9);
+                attemptToMove(goCount,j,random);
+            }
+        }
+    }
+
+    static void attemptToMove(long[] goCount,int goCount_index, int random){
+        if(random >= 4)
+            goCount[goCount_index]++;
     }
 }
